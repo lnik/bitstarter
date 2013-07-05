c78a012472e93d0cbc7763db88e3344d979f0ed5
lb <lb@ubuntu.(none)>
2013-07-05 21:25:20 +0200

    typo

--- web.js
+++ web.js
@@ -2,8 +2,6 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
-var str = fs.readFileSync('index.html').toString('utf-8');
-
 app.get('/', function(request, response) {
   response.send(fs.readFileSync('index.html').toString('utf-8'));
 });
