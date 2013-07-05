50269ccca5ecb151ce42eae02a88f8d661c9b28a
lb <lb@ubuntu.(none)>
2013-07-05 21:29:08 +0200

    not included

--- web.js
+++ web.js
@@ -1,9 +1,11 @@
 var express = require('express');
 
+var str = fs.readFileSync('index.html').toString('utf-8');
+
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send(fs.readFileSync('index.html').toString('utf-8'));
+  response.send(str);
 });
 
 var port = process.env.PORT || 5000;
